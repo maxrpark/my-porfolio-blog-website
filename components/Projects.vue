ffef9eb7349c89b9187513cc48adf97a932fd08f
Maxi Ruti Park <84664090+maxrpark@users.noreply.github.com>
2021-09-05 08:08:24 +0900

    Update Projects.vue

--- components/Projects.vue
+++ components/Projects.vue
@@ -54,13 +54,15 @@
                 <h6>{{ project.version }}</h6>
                 <h2>{{ project.name }}</h2>
               </div>
-              <div class="project-tags">
-                <div v-for="tag in project.tags.slice(0, 3)" :key="tag[0]">
-                  <div class="tag">
-                    <Nuxt-link class="" :to="`/projects/tags/${tag}`">{{
-                      tag
-                    }}</Nuxt-link>
-                  </div>
+               <div class="project-tags">
+                <div
+                  class="tag"
+                  v-for="tag in project.tags.slice(0, 3)"
+                  :key="tag[0]"
+                >
+                  <Nuxt-link class="" :to="`/projects/tags/${tag}`">{{
+                    tag
+                  }}</Nuxt-link>
                 </div>
               </div>
 
