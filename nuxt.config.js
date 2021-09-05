98369da960fcc6ba02e891a5b1df32b50ac988d9
Maxi Ruti Park <84664090+maxrpark@users.noreply.github.com>
2021-09-05 09:29:26 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
-  mode: 'universal',
-  ssr: ''true'
+  
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
