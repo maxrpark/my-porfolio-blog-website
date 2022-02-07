e07fecdff541d7fb9956694fc39b8d80f5428401
Maxi Ruti Park <84664090+maxrpark@users.noreply.github.com>
2022-02-07 09:13:24 +0900

    edits

--- pages/projects/tags/_tag.vue
+++ pages/projects/tags/_tag.vue
@@ -20,6 +20,7 @@
               <div class="project-title">
                 <h5>{{ project.version }}</h5>
                 <h2>{{ project.name }}</h2>
+              </div>
                 <div class="project-tags">
                   <div v-for="tag in project.tags.slice(0, 3)"
                   :key="tag[0]">
